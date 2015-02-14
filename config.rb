286ca287581b0d9d848979be8688d3ad3d5f50bd
Wellington Mitrut <wellingtonmitrut@gmail.com>
2015-02-14 21:06:41 -0200

    First configs

--- config.rb
+++ config.rb
@@ -1,72 +1,91 @@
-###
-# Compass
-###
+# View Middleman configurations:
+# http://localhost:4567/__middleman/config/
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
 
-###
-# Page options, layouts, aliases and proxies
-###
+# Site Settings
+# ----------------------------------------------
+@google_analytics = ''
+set :site_name, 'Wmitrut middleman boilerplate'
+set :site_description, 'A Static Site Generator'
+set :site_twitter, '@wmitrut'
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Slim HTML
+# ----------------------------------------------
+::Slim::Engine.set_options :format  => :html
 
-###
-# Helpers
-###
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+# i18n
+# ----------------------------------------------
+activate :i18n, :mount_at_root => :'pt-BR'
 
+
+# Livereload
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+# ----------------------------------------------
+configure :development do
+  activate :livereload, :no_swf => true
+end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
-set :css_dir, 'stylesheets'
+# Page options, layouts, aliases and proxies
+# ----------------------------------------------
 
-set :js_dir, 'javascripts'
 
-set :images_dir, 'images'
+# Bower Config
+# ----------------------------------------------
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  @bower_assets_path = File.join "#{root}", @bower_config["directory"]
+  sprockets.append_path @bower_assets_path
+end
+
+
+# Configure assets directories
+# ----------------------------------------------
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+set :partials_dir, 'shared'
+
+
+# Other configurations
+# ----------------------------------------------
+set :trailing_slash, false
+
+
+# Development-specific configuration
+# ----------------------------------------------
+configure :development do
+  activate :directory_indexes
+  set :debug_assets, true
+  ::Slim::Engine.set_options :pretty => true
+end
+
 
 # Build-specific configuration
+# ----------------------------------------------
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  # Use relative URLs
+  activate :directory_indexes
+
+  # Activate gzip
+  activate :gzip
+
+  # Minify CSS on build
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Add asset fingerprinting to avoid cache issues
+  activate :asset_hash
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :cache_buster
 
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # Compress PNGs after build (First: gem install middleman-smusher)
+  require "middleman-smusher"
+  activate :smusher
 end
